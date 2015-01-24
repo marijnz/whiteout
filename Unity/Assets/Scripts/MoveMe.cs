7ff91cd3cf30babc68de61337cd2b66134677745
Arco Mul <me@arcomul.nl>
2015-01-24 16:42:12 +0100

    Character in level and collides with level and objects

--- Unity/Assets/Scripts/MoveMe.cs
+++ Unity/Assets/Scripts/MoveMe.cs
@@ -16,10 +16,17 @@ public class MoveMe : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float moveX = CustomInputManager.GetAxis (CustomInputManager.Token.HorizontalMove, 1);
-		float moveY = CustomInputManager.GetAxis (CustomInputManager.Token.VerticalMove, 1);
-		transform.Translate (moveX, moveY, 0);
-
+        float moveX = CustomInputManager.GetAxis(CustomInputManager.Token.HorizontalMove, 1) * moveSpeed * Time.deltaTime;
+        float moveY = CustomInputManager.GetAxis(CustomInputManager.Token.VerticalMove, 1) * moveSpeed * Time.deltaTime;
+        if (moveX != 0 || moveY != 0)
+        {
+            //GetComponent<Rigidbody2D>().AddForce(new Vector3(moveX, moveY));
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveX, moveY);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
 		if(CustomInputManager.ButtonGotPressed(CustomInputManager.Token.Interact, 1)){
 			print ("What do we do now?");
 		}
