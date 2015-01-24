82b362303915ad8f1f4413847168ef2d29f38525
Arco Mul <me@arcomul.nl>
2015-01-25 00:32:08 +0100

    Blood spatters

--- Unity/Assets/Scripts/MoveMe.cs
+++ Unity/Assets/Scripts/MoveMe.cs
@@ -10,21 +10,31 @@ public class MoveMe : MonoBehaviour {
 	KeyCode moveLeft = KeyCode.A;
 	private int impendingDoom = 0;
 	private float angle;
-    public int MaxHits = 50;	
-	// Use this for initialization
+    public int MaxHits = 50;
+    public GameObject BloodSpatter;
+
 	void Start () {
 		
 	}
-	void OnCollisionEnter2D(Collision2D collision){
+	void OnCollisionEnter2D(Collision2D collision)
+    {
 		impendingDoom++;
 
         HitpointManager.Instance.SpawnHitPoint(collision.contacts[0].point);
+
+        GameObject blood = (GameObject)Instantiate(BloodSpatter);
+        blood.transform.position = new Vector3(collision.contacts[0].point.x, collision.contacts[0].point.y, -0.2f);
+        Vector2 direction = new Vector2(transform.position.x, transform.position.y) - collision.contacts[0].point;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        blood.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 	}
 
 
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         float moveX = CustomInputManager.GetAxis(CustomInputManager.Token.HorizontalMove, 1) * moveSpeed * Time.deltaTime;
         float moveY = CustomInputManager.GetAxis(CustomInputManager.Token.VerticalMove, 1) * moveSpeed * Time.deltaTime;
         if (moveX != 0 || moveY != 0)
@@ -36,8 +46,10 @@ public class MoveMe : MonoBehaviour {
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
-		if(GetComponent<Rigidbody2D>().velocity.x != 0.0f || GetComponent<Rigidbody2D>().velocity.y != 0.0f)
-			angle = Mathf.Atan2 (moveX, -moveY) * Mathf.Rad2Deg;
+        if (GetComponent<Rigidbody2D>().velocity.x != 0.0f || GetComponent<Rigidbody2D>().velocity.y != 0.0f)
+        {
+            angle = Mathf.Atan2(moveX, -moveY) * Mathf.Rad2Deg;
+        }
 		GetComponent<Rigidbody2D> ().rotation = angle;
 		if(CustomInputManager.ButtonGotPressed(CustomInputManager.Token.Interact, 1)){
 			print ("What do we do now?");
