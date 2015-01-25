5bc12b0a457f90834aba3f67ba61461b650ad10d
Arco Mul <me@arcomul.nl>
2015-01-25 15:29:37 +0100

    Better collision and bloodsplatter

--- Unity/Assets/Scripts/Avatar.cs
+++ Unity/Assets/Scripts/Avatar.cs
@@ -39,7 +39,7 @@ public class Avatar : MonoBehaviour {
             {
                 AvatarAnimator.triggerDeathAnim();
                 dead = true;
-                GetComponent<BoxCollider2D>().enabled = false;
+                GetComponent<CircleCollider2D>().enabled = false;
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 GetComponent<MoveMe>().enabled = false;
             }
