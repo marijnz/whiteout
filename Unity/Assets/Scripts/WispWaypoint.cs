d9302a597a3d8807a0ca1b8fea26ce9df8d9ebd1
Arco Mul <me@arcomul.nl>
2015-01-25 13:53:20 +0100

    Wisps!!!!

---
+++ Unity/Assets/Scripts/WispWaypoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WispWaypoint : MonoBehaviour {
+
+    public WispWaypoint[] connectedWaypoints;
+
+    public Vector3 getPosition ()
+    {
+        return transform.position;
+    }
+
+    public WispWaypoint getNextWaypoint ()
+    {
+        return connectedWaypoints[Mathf.FloorToInt(Random.value * connectedWaypoints.Length)];
+    }
+}
